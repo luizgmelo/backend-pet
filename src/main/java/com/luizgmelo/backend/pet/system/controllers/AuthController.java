08a7215ba01e9f703bc2775737dd7d887bff991d
Luiz Guilherme <luizgmelo64@gmail.com>
2025-05-16 11:58:42 -0300

    refac: add user role as default when register

--- src/main/java/com/luizgmelo/backend/pet/system/controllers/AuthController.java
+++ src/main/java/com/luizgmelo/backend/pet/system/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.luizgmelo.backend.pet.system.controllers;
 
 import com.luizgmelo.backend.pet.system.dto.*;
+import com.luizgmelo.backend.pet.system.enums.UserRole;
 import com.luizgmelo.backend.pet.system.infra.security.TokenService;
 import com.luizgmelo.backend.pet.system.models.UserModel;
 import com.luizgmelo.backend.pet.system.repositories.UserRepository;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -50,7 +52,7 @@ public class AuthController {
         newUser.setName(body.name());
         newUser.setEmail(body.email());
         newUser.setPassword(passwordHashed);
-        newUser.setRoles(body.roles());
+        newUser.setRoles(Set.of(UserRole.USER));
         userRepository.save(newUser);
         RegisterResponseDTO response = new RegisterResponseDTO("Usuário cadastrado com sucesso");
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
--- src/main/java/com/luizgmelo/backend/pet/system/dto/RegisterRequestDTO.java
+++ src/main/java/com/luizgmelo/backend/pet/system/dto/RegisterRequestDTO.java
@@ -1,8 +1,4 @@
 package com.luizgmelo.backend.pet.system.dto;
 
-import com.luizgmelo.backend.pet.system.enums.UserRole;
-
-import java.util.Set;
-
-public record RegisterRequestDTO(String name, String email, String password, Set<UserRole> roles) {
+public record RegisterRequestDTO(String name, String email, String password) {
 }
