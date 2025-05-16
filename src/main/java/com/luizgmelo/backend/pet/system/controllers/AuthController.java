7252a2964e5fec9336f58be437e5d3ead24417c3
Luiz Guilherme <luizgmelo64@gmail.com>
2025-05-16 12:10:28 -0300

    refac: put login and register logic in user service

--- src/main/java/com/luizgmelo/backend/pet/system/controllers/AuthController.java
+++ src/main/java/com/luizgmelo/backend/pet/system/controllers/AuthController.java
@@ -1,19 +1,12 @@
 package com.luizgmelo.backend.pet.system.controllers;
 
 import com.luizgmelo.backend.pet.system.dto.*;
-import com.luizgmelo.backend.pet.system.enums.UserRole;
-import com.luizgmelo.backend.pet.system.infra.security.TokenService;
-import com.luizgmelo.backend.pet.system.models.UserModel;
-import com.luizgmelo.backend.pet.system.repositories.UserRepository;
 import com.luizgmelo.backend.pet.system.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -21,40 +14,23 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserRepository userRepository;
-
-    private final PasswordEncoder passwordEncoder;
-
-    private final TokenService tokenService;
-
     private final UserService userService;
 
-    // TODO Create UserService for business logic
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO body) {
-        UserModel user = userRepository.findByEmail(body.email()).orElseThrow(()-> new RuntimeException("Login ou Senha incorreto"));
-        if (!passwordEncoder.matches(body.password(), user.getPassword())) {
+        LoginResponseDTO response = userService.login(body);
+        if (response == null) {
             return ResponseEntity.badRequest().build();
         }
-        String token = tokenService.generateToken(user);
-        LoginResponseDTO response = new LoginResponseDTO(token);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO body) {
-        Optional<UserModel> user = userRepository.findByEmail(body.email());
-        if (user.isPresent()) {
+        RegisterResponseDTO response = userService.register(body);
+        if (response == null) {
             return ResponseEntity.badRequest().build();
         }
-        String passwordHashed = passwordEncoder.encode(body.password());
-        UserModel newUser = new UserModel();
-        newUser.setName(body.name());
-        newUser.setEmail(body.email());
-        newUser.setPassword(passwordHashed);
-        newUser.setRoles(Set.of(UserRole.USER));
-        userRepository.save(newUser);
-        RegisterResponseDTO response = new RegisterResponseDTO("Usuário cadastrado com sucesso");
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
--- src/main/java/com/luizgmelo/backend/pet/system/services/UserService.java
+++ src/main/java/com/luizgmelo/backend/pet/system/services/UserService.java
@@ -1,23 +1,56 @@
 package com.luizgmelo.backend.pet.system.services;
 
-import com.luizgmelo.backend.pet.system.dto.UserDTO;
+import com.luizgmelo.backend.pet.system.dto.*;
+import com.luizgmelo.backend.pet.system.enums.UserRole;
+import com.luizgmelo.backend.pet.system.infra.security.TokenService;
 import com.luizgmelo.backend.pet.system.models.UserModel;
 import com.luizgmelo.backend.pet.system.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final PasswordEncoder passwordEncoder;
+
+    private final TokenService tokenService;
+
 
     public UserDTO findById(UUID id) {
         UserModel user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
         return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getRoles());
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO dto) {
+        UserModel user = userRepository.findByEmail(dto.email()).orElseThrow(()-> new RuntimeException("Login ou Senha incorreto"));
+        if (!passwordEncoder.matches(dto.password(), user.getPassword())) {
+            return null;
+        }
+        String token = tokenService.generateToken(user);
+        return new LoginResponseDTO(token);
+    }
+
+    public RegisterResponseDTO register(RegisterRequestDTO dto) {
+        Optional<UserModel> user = userRepository.findByEmail(dto.email());
+        if (user.isPresent()) {
+            return null;
+        }
+        String passwordHashed = passwordEncoder.encode(dto.password());
+        UserModel newUser = new UserModel();
+        newUser.setName(dto.name());
+        newUser.setEmail(dto.email());
+        newUser.setPassword(passwordHashed);
+        newUser.setRoles(Set.of(UserRole.USER));
+        userRepository.save(newUser);
+        return new RegisterResponseDTO("Usuário cadastrado com sucesso");
     }
 
 
