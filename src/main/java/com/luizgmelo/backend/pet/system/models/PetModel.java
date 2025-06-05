48955efefa1a8e88af95d20fa6600bf22714b901
Luiz Guilherme <luizgmelo64@gmail.com>
2025-06-05 12:25:51 -0300

    refac: add createdAt column in PetModel

--- src/main/java/com/luizgmelo/backend/pet/system/models/PetModel.java
+++ src/main/java/com/luizgmelo/backend/pet/system/models/PetModel.java
@@ -1,12 +1,21 @@
 package com.luizgmelo.backend.pet.system.models;
 
+import com.luizgmelo.backend.pet.system.enums.PetSex;
 import com.luizgmelo.backend.pet.system.enums.PetType;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "tb_pet")
+@Getter
+@Setter
 public class PetModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -15,7 +24,7 @@ public class PetModel {
     private String firstName;
     private String lastName;
     private PetType type;
-    private Character sex;
+    private PetSex sex;
     private Integer age;
     private Double weight;
     private String breed;
@@ -23,4 +32,7 @@ public class PetModel {
     @ManyToOne
     @JoinColumn(name = "address_id")
     private AddressModel address;
+
+    @CreatedDate
+    private Instant createdAt;
 }
