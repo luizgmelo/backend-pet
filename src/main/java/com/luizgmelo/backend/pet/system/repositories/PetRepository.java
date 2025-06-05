dfb17fe939ed896ebd53ef2d0746ef0575745a17
Luiz Guilherme <luizgmelo64@gmail.com>
2025-06-05 12:03:03 -0300

    feat: create pet repository

---
+++ src/main/java/com/luizgmelo/backend/pet/system/repositories/PetRepository.java
@@ -0,0 +1,11 @@
+package com.luizgmelo.backend.pet.system.repositories;
+
+import com.luizgmelo.backend.pet.system.models.PetModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PetRepository extends JpaRepository<PetModel, UUID> {
+}
